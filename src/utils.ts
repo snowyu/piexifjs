eb0e8fd60a1840d962f454131884ce19dd9422d8
Riceball LEE <snowyu.lee@gmail.com>
2020-04-17 21:28:37 +0800

    fix: undefined show be any type, just fix it pack as number

--- src/utils.ts
+++ src/utils.ts
@@ -436,7 +436,14 @@ export const _toRational = (
 };
 
 export const _toUndefined = (rawValue: string, offset: number): ITagBinary => {
-  if (typeof rawValue !== "string") {
+  if (typeof rawValue === 'number') {
+    if (Number.isSafeInteger(rawValue)) {
+      return _toLong(rawValue, offset);
+    } else {
+      // TODO: howto express the max long number and check, bigInt?
+      return _toRational([Math.trunc(Math.abs(rawValue * 100)), 100], offset);
+    }
+  } else if (typeof rawValue !== "string") {
     const t = Array.isArray(rawValue) ? "Array" : typeof rawValue;
     throw new exceptions.ValueConvertError(
       `Value must be "string". Got "${t}".`
